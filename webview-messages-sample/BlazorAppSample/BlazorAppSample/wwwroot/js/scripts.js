e77c61fab77df904623fa7579009391daf875ec1
nich <nicobaccy@icloud.com>
2024-02-11 13:18:53 +0100

    Add message send from blazor

--- webview-messages-sample/BlazorAppSample/BlazorAppSample/wwwroot/js/scripts.js
+++ webview-messages-sample/BlazorAppSample/BlazorAppSample/wwwroot/js/scripts.js
@@ -4,16 +4,24 @@ var webViewMessageHelperRef;
 function initializeWebViewEvents() {
     // See https://learn.microsoft.com/en-us/microsoft-edge/webview2/get-started/wpf#step-10---communication-between-host-and-web-content
     // and https://learn.microsoft.com/en-us/microsoft-edge/webview2/reference/javascript/webview
-    // This API is avaiable only when accessing the Blazor app from a webiew. If for any reason this is not available, as alternative you can use direct JS functions instead of messages, see https://learn.microsoft.com/en-us/microsoft-edge/webview2/get-started/wpf#step-9---scripting
+    // This API is available only when accessing the Blazor app from a webiew. If for any reason this is not available, as alternative you can use direct JS functions instead of messages, see https://learn.microsoft.com/en-us/microsoft-edge/webview2/get-started/wpf#step-9---scripting
     if (window.chrome.webview) {
         window.chrome.webview.addEventListener('message', receiveMessageFromWebView);
     }
 }
 
-// Initializes the helper reference needed to interop with dotnet.
+// Initializes the helper reference required to interop with dotnet.
 function initializeDotNetMessagesHelper(helperRef) {
     webViewMessageHelperRef = helperRef;
     console.log("[interop] Initialized messages helper");
+}
+
+// Sends the message to the webview.
+function sendMessageToWebView(message) {
+    // This API is available only when accessing the Blazor app from a webiew. If for any reason this is not available, as alternative you can use direct JS functions instead of messages, see https://learn.microsoft.com/en-us/microsoft-edge/webview2/get-started/wpf#step-9---scripting
+    if (window.chrome.webview) {
+        window.chrome.webview.postMessage(message);
+    }
 }
 
 // This one is invoked by the webview that is accessing the Blazor app.
