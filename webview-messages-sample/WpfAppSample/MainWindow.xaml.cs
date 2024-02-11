88fa1501acb8ccdc2354c64eeb12e37c8dec04b8
Nicolò Baccin <98348348+nich2408@users.noreply.github.com>
2024-02-11 13:24:53 +0100

    Add message from blazor (#1)
    * Add message send from blazor

--- webview-messages-sample/WpfAppSample/MainWindow.xaml.cs
+++ webview-messages-sample/WpfAppSample/MainWindow.xaml.cs
@@ -19,21 +19,30 @@ namespace WpfAppSample
         public MainWindow()
         {
             InitializeComponent();
-            _ = InitializeAsync();
+            _ = InitializeWebViewAsync();
         }
 
-        private async Task InitializeAsync()
+        private async Task InitializeWebViewAsync()
         {
             await webView.EnsureCoreWebView2Async(null);
+            // Handle receiving messages from webview.
+            webView.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+        }
+
+        private void CoreWebView2_WebMessageReceived(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            txbReceivedMsg.Text = e.TryGetWebMessageAsString();
         }
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
+            // This example posts a string, you can also post JSON.
             webView.CoreWebView2.PostWebMessageAsString(txbMsg.Text);
         }
 
-        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        private async void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
+            await this.webView.CoreWebView2.Profile.ClearBrowsingDataAsync();
             webView.CoreWebView2.Reload();
         }
     }
